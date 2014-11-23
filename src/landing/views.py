a088355fb8f8d678fa5c7b6e000925a4832ad9f1
Edu Herraiz <gshark@gmail.com>
2014-11-23 20:33:46 +0100

    Número interesados

--- src/landing/views.py
+++ src/landing/views.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.shortcuts import redirect
 from django.views.generic import CreateView, TemplateView
 from django.core.urlresolvers import reverse_lazy
 
 from models import Interesado
 from forms import InteresadoForm
+import random
 
 
 class HomeView(CreateView):
@@ -16,6 +14,10 @@ class HomeView(CreateView):
     form_class = InteresadoForm
     success_url = reverse_lazy('gracias')
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['number'] = Interesado.objects.count()
+        return context
 
 class GraciasView(TemplateView):
     template_name = 'gracias.html'
